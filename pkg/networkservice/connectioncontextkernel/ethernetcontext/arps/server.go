09ed0368e16bbddbe2088285851a06832f5692e2
denis-tingajkin <49399980+denis-tingajkin@users.noreply.github.com>
2020-01-28 21:22:12 +0700

    add kernel client for setting arps (#46)
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

---
+++ pkg/networkservice/connectioncontextkernel/ethernetcontext/arps/server.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package arps provides networkservice chain elements for setting the arp entries for kernel linux config
+package arps
+
+import (
+	"context"
+	"strings"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/ligato/vpp-agent/api/models/linux"
+	"github.com/networkservicemesh/api/pkg/api/connection"
+	"github.com/networkservicemesh/api/pkg/api/connection/mechanisms/kernel"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+
+	"github.com/networkservicemesh/sdk-vppagent/pkg/networkservice/vppagent"
+)
+
+type setKernelArpsServer struct{}
+
+// NewServer provides a NetworkServiceServer that sets the arp entry for kernel linux config
+func NewServer() networkservice.NetworkServiceServer {
+	return &setKernelArpsServer{}
+}
+
+func (s *setKernelArpsServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*connection.Connection, error) {
+	config := vppagent.Config(ctx)
+	if mechanism := kernel.ToMechanism(request.GetConnection().GetMechanism()); mechanism != nil && len(config.LinuxConfig.Interfaces) > 0 {
+		iface := config.LinuxConfig.Interfaces[len(config.LinuxConfig.Interfaces)-1]
+		if request.GetConnection().GetContext().GetEthernetContext().GetDstMac() != "" &&
+			request.GetConnection().GetContext().GetIpContext().GetDstIpAddr() != "" {
+			config.LinuxConfig.ArpEntries = append(config.LinuxConfig.ArpEntries, &linux.ARPEntry{
+				IpAddress: strings.Split(request.GetConnection().GetContext().IpContext.GetDstIpAddr(), "/")[0],
+				Interface: iface.Name,
+				HwAddress: request.Connection.GetContext().EthernetContext.GetDstMac(),
+			})
+		}
+	}
+	return next.Server(ctx).Request(ctx, request)
+}
+
+func (s *setKernelArpsServer) Close(ctx context.Context, conn *connection.Connection) (*empty.Empty, error) {
+	config := vppagent.Config(ctx)
+	if mechanism := kernel.ToMechanism(conn.GetMechanism()); mechanism != nil && len(config.LinuxConfig.Interfaces) > 0 {
+		iface := config.LinuxConfig.Interfaces[len(config.LinuxConfig.Interfaces)-1]
+		if conn.GetContext().GetEthernetContext().GetDstMac() != "" &&
+			conn.GetContext().GetIpContext().GetDstIpAddr() != "" {
+			config.LinuxConfig.ArpEntries = append(config.LinuxConfig.ArpEntries, &linux.ARPEntry{
+				IpAddress: strings.Split(conn.GetContext().IpContext.DstIpAddr, "/")[0],
+				Interface: iface.Name,
+				HwAddress: conn.GetContext().EthernetContext.GetDstMac(),
+			})
+		}
+	}
+	return next.Server(ctx).Close(ctx, conn)
+}
---
+++ pkg/networkservice/connectioncontextkernel/ethernetcontext/arps/server_test.go
@@ -0,0 +1,104 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package arps
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/ligato/vpp-agent/api/models/linux"
+	"github.com/networkservicemesh/api/pkg/api/connection"
+	"github.com/networkservicemesh/api/pkg/api/connection/mechanisms/kernel"
+	"github.com/networkservicemesh/api/pkg/api/connectioncontext"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/networkservicemesh/sdk-vppagent/pkg/networkservice/vppagent"
+)
+
+func TestClientBasic(t *testing.T) {
+	request := &networkservice.NetworkServiceRequest{
+		Connection: &connection.Connection{
+			Id: "1",
+			Mechanism: &connection.Mechanism{
+				Type: kernel.MECHANISM,
+			},
+			Context: &connectioncontext.ConnectionContext{
+				EthernetContext: &connectioncontext.EthernetContext{
+					DstMac: "0a-1b-3c-4d-5e-6f",
+				},
+				IpContext: &connectioncontext.IPContext{
+					DstIpAddr: "172.16.1.2",
+				},
+			},
+		},
+	}
+	server := next.NewNetworkServiceServer(vppagent.NewServer(), &testingServer{t}, NewServer())
+	_, _ = server.Request(context.Background(), request)
+	_, _ = server.Close(context.Background(), request.GetConnection())
+}
+
+type testingServer struct {
+	*testing.T
+}
+
+func (t *testingServer) Request(ctx context.Context, in *networkservice.NetworkServiceRequest) (*connection.Connection, error) {
+	config := vppagent.Config(ctx)
+	assert.NotNil(t, config)
+	config.LinuxConfig = &linux.ConfigData{
+		Interfaces: []*linux.Interface{
+			{
+				Name: "client-1",
+			},
+		},
+	}
+	conn, err := next.Server(ctx).Request(ctx, in)
+	assert.Nil(t, err)
+	expectedArp := &linux.ARPEntry{
+		Interface: config.LinuxConfig.Interfaces[0].Name,
+		IpAddress: in.GetConnection().GetContext().GetIpContext().GetDstIpAddr(),
+		HwAddress: in.GetConnection().GetContext().GetEthernetContext().GetDstMac(),
+	}
+	assert.Equal(t, 1, len(config.LinuxConfig.ArpEntries))
+	assert.Equal(t, expectedArp, config.LinuxConfig.ArpEntries[0])
+	return conn, err
+}
+
+func (t *testingServer) Close(ctx context.Context, conn *connection.Connection) (*empty.Empty, error) {
+	config := vppagent.Config(ctx)
+	assert.NotNil(t, config)
+	targetInterface := &linux.Interface{
+		Name: "SRC-1",
+	}
+	config.LinuxConfig = &linux.ConfigData{
+		Interfaces: []*linux.Interface{
+			targetInterface,
+		},
+	}
+	result, err := next.Server(ctx).Close(ctx, conn)
+	assert.Nil(t, err)
+	expectedArp := &linux.ARPEntry{
+		Interface: config.LinuxConfig.Interfaces[0].Name,
+		IpAddress: conn.GetContext().GetIpContext().GetDstIpAddr(),
+		HwAddress: conn.GetContext().GetEthernetContext().GetDstMac(),
+	}
+	assert.Equal(t, 1, len(config.LinuxConfig.ArpEntries))
+	assert.Equal(t, expectedArp, config.LinuxConfig.ArpEntries[0])
+	return result, err
+}
